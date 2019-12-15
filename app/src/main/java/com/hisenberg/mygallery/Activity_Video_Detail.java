418e881e9c1e235c02bf0acbfba430ab6e76e20a
Athar <Atharm621@gmail.com>
2019-12-16 02:08:54 +0800

    Remove Unuse

--- app/src/main/java/com/hisenberg/mygallery/Activity_Video_Detail.java
+++ app/src/main/java/com/hisenberg/mygallery/Activity_Video_Detail.java
@@ -1,49 +1,36 @@
 package com.hisenberg.mygallery;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.media.AudioManager;
-import android.media.MediaMetadataRetriever;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.google.android.exoplayer2.ExoPlaybackException;
-import com.google.android.exoplayer2.ExoPlayer;
-import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.PlaybackParameters;
-import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
-import com.google.android.exoplayer2.source.LoopingMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.ProgressiveMediaSource;
 import com.google.android.exoplayer2.source.TrackGroupArray;
-import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
-import com.google.android.exoplayer2.trackselection.TrackSelection;
 import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.ui.AspectRatioFrameLayout;
 import com.google.android.exoplayer2.ui.PlayerView;
-import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DataSource;
-import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
-import com.google.android.exoplayer2.util.RepeatModeUtil;
 import com.google.android.exoplayer2.util.Util;
 
 import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity_Video_Detail extends AppCompatActivity {
 
@@ -83,9 +70,7 @@ public class Activity_Video_Detail extends AppCompatActivity {
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (Util.SDK_INT >= 24) {
                     releasePlayer();
-                }
                 finish();
             }
         });
@@ -113,6 +98,7 @@ public class Activity_Video_Detail extends AppCompatActivity {
         playerView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
+                view.performClick();
                 return mDetector.onTouchEvent(motionEvent);
             }
         });
@@ -144,16 +130,14 @@ public class Activity_Video_Detail extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
-        if (Util.SDK_INT >= 24) {
             initializePlayer();
-        }
     }
 
     @Override
     public void onResume() {
         super.onResume();
         hideSystemUi();
-        if ((Util.SDK_INT < 24 || player == null)) {
+        if ( player == null) {
             initializePlayer();
         }
     }
@@ -165,24 +149,19 @@ public class Activity_Video_Detail extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        if (Util.SDK_INT < 24) {
             releasePlayer();
-        }
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        if (Util.SDK_INT >= 24) {
             releasePlayer();
-        }
     }
 
     private void releasePlayer() {
@@ -252,9 +231,9 @@ public class Activity_Video_Detail extends AppCompatActivity {
     private class MyGestureListner implements GestureDetector.OnGestureListener {
         private static final int SWIPE_MIN_DISTANCE = 120;
         private static final int SWIPE_THRESHOLD_VELOCITY = 200;
-        AudioManager audioManager = null;
+        AudioManager audioManager;
 
-        public MyGestureListner() {
+        MyGestureListner() {
             audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
         }
 
